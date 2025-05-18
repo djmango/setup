3c5425d42d75c14e20e2e2b739dca7d373ad0c65
Sulaiman Khan Ghori <sulaiman.ghori@outlook.com>
2025-05-17 17:59:51 -0700

    Basic setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt install git curl magic-wormhole
+curl -s https://install.zerotier.com | sudo bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install bob-nvim
+cargo install cargo-binstall
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
